a8919b2cfc786867b92b2816b6fa1bfa2ca780f6
Emma Willmann <emma.anne.81@gmail.com>
2018-12-13 07:27:04 -0600

    another

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use((err, req, res, next) => {
 // Serve Static assets if in productionc
 if(process.env.NODE_ENV === 'production'){
   //set static folder
-  app.use(express.static("./client/build"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
