13de30baa8d1b6edfa5fe47e9af6f8d3bc102817
Emma Willmann <emma.anne.81@gmail.com>
2018-12-13 11:48:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -86,7 +86,6 @@ if(process.env.NODE_ENV === 'production'){
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    console.log(__dirname);
   });
 }
 
