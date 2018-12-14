a63d723ba5b2e618d94fb149152e9155e5cdfbfa
Emma Willmann <emma.anne.81@gmail.com>
2018-12-13 18:54:52 -0600

    another

--- app.js
+++ app.js
@@ -59,6 +59,16 @@ app.use('/api', routes);
  // });
 //});
 
+// Serve Static assets if in productionc
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // send 404 if no other route matched
 app.use((req, res) => {
   res.status(404).json({
@@ -79,15 +89,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-// Serve Static assets if in productionc
-//if(process.env.NODE_ENV === 'production'){
-  //set static folder
-  app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-//}
 console.log('oh no');
 
 
